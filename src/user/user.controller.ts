dc8696803eab734faf4d2438f45a1eaa651146a0
joudialkabbani <joudi.kab01@gmail.com>
2023-09-25 17:47:30 +0300

    custom user decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,24 @@
 import {
   Controller,
   Get,
-  Req,
+  Patch,
   UseGuards,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(
+    @GetUser() user: User,
+    // @GetUser('email') email: string,
+  ) {
+    // console.log({ email });
+    return user;
   }
+  @Patch()
+  editUser() {}
 }
